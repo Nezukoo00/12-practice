a15c43ef1e395b348dd4c2edcb9e241ef9044f6d
Виктория Погиба <viktoruapogiba@gmail.com>
2024-03-31 19:40:33 +0300

    Добавьте файлы проекта.

---
+++ 13 pexbksa/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const string filePath = @"C:\Users\админ\OneDrive\Документы\30000 strings.txt";
+        const string userFile = @"C:\Users\админ\OneDrive\Документы\user_data.txt";
+
+        // Считывание файла
+        var lines = await ReadFileAsync(filePath);
+
+        // Интерфейс пользователя
+        Console.WriteLine("Write Name:");
+        var firstName = Console.ReadLine();
+
+        Console.WriteLine("Write Surname:");
+        var lastName = Console.ReadLine();
+
+        // Печать данных
+        Console.WriteLine("Print data? (y/n)");
+        var printData = Console.ReadLine().ToLower() == "y";
+
+        if (printData)
+        {
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            File.WriteAllText(userFile, $"{firstName} {lastName}");
+            Console.WriteLine("User data saved to: {0}", userFile);
+        }
+    }
+
+    static async Task<string[]> ReadFileAsync(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        {
+            var lines = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                lines.Add(await reader.ReadLineAsync());
+            }
+            return lines.ToArray();
+        }
+    }
+}
